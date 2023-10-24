79a0f7662a6e00dfa61d5a8aadac1b0760776210
chanw12 <wooju9812127@gmail.com>
2023-10-24 11:21:58 +0900

    종료 명령어 등록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명령을 입력하세요: ");
+        String cmd  = scanner.next();
+        if(cmd == "종료"){
+            return;
+        }
+
     }
 }
