26a7fdc318e133f1632057862d1910caebe5bc2d
chanw12 <wooju9812127@gmail.com>
2023-10-24 13:39:01 +0900

    존재하지않는 명언 삭제에 대한 처리

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         List<FamousSaying> famousSayingList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("명령을 입력하세요: ");
+        System.out.print("명령)");
         String cmd  = scanner.nextLine();
         while(!cmd.equals("종료")){
             if(cmd.equals("등록")){
@@ -24,7 +24,7 @@ public class Main {
                 famousSayingList.add(fs);
 
                 System.out.println(fs.id +"번 명언이 등록되었습니다.");
-                System.out.print("명령을 입력하세요: ");
+                System.out.print("명령)");
                 cmd  = scanner.nextLine();
             }
             else if(cmd.equals("목록")){
@@ -35,24 +35,31 @@ public class Main {
                     System.out.println(f.id + "  /  " +f.author + "  /  " +f.f_text);
                 }
 
-                System.out.print("명령을 입력하세요: ");
+                System.out.print("명령)");
                 cmd  = scanner.nextLine();
             }
             else if(cmd.contains("삭제")){
-                System.out.println(cmd);
                 Pattern pattern = Pattern.compile("(\\d+)");
                 Matcher matcher = pattern.matcher(cmd);
-//
+                boolean found = false;
                 if(matcher.find()){
                     String num = matcher.group();
                     int nNum = Integer.parseInt(num);
                     for(int k =0;k<famousSayingList.size();k++){
                         if(famousSayingList.get(k).id == nNum){
                             famousSayingList.remove(k);
+                            found = true;
+                            break;
                         }
                     }
+                    if(!found){
+                        System.out.println(nNum+"번 명언은 존재하지 않습니다.");
+                    }else{
+                        System.out.println(nNum+ "번 명언이 삭제되었습니다.");
+                    }
+
                 }
-                System.out.print("명령을 입력하세요: ");
+                System.out.print("명령)");
                 cmd  = scanner.nextLine();
             }
 
