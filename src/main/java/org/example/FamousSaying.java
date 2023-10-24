15b07612a591ea25289fd03d8c15eaf5a2ee7226
chanw12 <wooju9812127@gmail.com>
2023-10-24 11:43:16 +0900

    명언등록 기능

---
+++ src/main/java/org/example/FamousSaying.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FamousSaying {
+    public static int idVal= 1;
+    int id;
+    String text;
+    String author;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<FamousSaying> famousSayingList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.print("명령을 입력하세요: ");
         String cmd  = scanner.next();
-        if(cmd == "종료"){
-            return;
+        while(!cmd.equals("종료")){
+            if(cmd.equals("등록")){
+                FamousSaying fs = new FamousSaying();
+                System.out.print("명언  :  ");
+                fs.text = scanner.nextLine();
+                scanner.nextLine();
+                System.out.print("작가  :  ");
+                fs.author = scanner.nextLine();
+
+                fs.id = FamousSaying.idVal++;
+                famousSayingList.add(fs);
+                System.out.println(fs.id +"번 명언이 등록되었습니다.");
+                System.out.print("명령을 입력하세요: ");
+                cmd  = scanner.next();
+            }
+
         }
+        return;
+
+
 
     }
 }
